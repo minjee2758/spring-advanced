0e1056f1e96f54f622b56aacc70b671698d731a3
minjee2758 <minjee2758@naver.com>
2025-04-20 22:14:44 +0900

    레벨 1-3 : Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,12 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    /*
+    @Pattern식으로 새로운 비밀번호로 바꿀 때 제약조건 설정
+     */
+
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$", message = "새 비밀번호는 8자 이상이며 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
