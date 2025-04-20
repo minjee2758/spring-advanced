4bf689961375a4a0309f2a27e4b85eaf8e93b7ee
minjee2758 <minjee2758@naver.com>
2025-04-20 22:12:27 +0900

    Revert "refactor : Validation - Dto에서  Pattern식 적용"
    This reverts commit 82a9190f9dfab88f3c374335ef99c5b05012a924.

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,7 +1,6 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +12,6 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-
-    /*
-    @Pattern식으로 새로운 비밀번호로 바꿀 떄 제약조건 설정
-     */
-
     @NotBlank
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$", message = "새 비밀번호는 8자 이상이며 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
